709a2b1c74664d5f3f9f8a0ab0d117ed27a758dc
Maroon Gecko <pawarkartik0@gmail.com>
2026-04-16 15:11:20 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 # Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+  list_display = ('creator','content')
+  list_filter = ("creator",)
+
 
 class PostAdmin(admin.ModelAdmin): 
   prepopulated_fields = {"slug": ("title",)}
@@ -11,3 +16,4 @@ class PostAdmin(admin.ModelAdmin):
 
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment,CommentAdmin)
