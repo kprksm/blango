6c12fcbc571c4aef32842f0dfbd9c39f2f4ad25d
Maroon Gecko <pawarkartik0@gmail.com>
2026-05-13 06:55:50 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = [
   'barcodeisabel-doubleflood-8000.codio.io',
   'caviarcopper-logocrater-8000.codio.io',
   'volumemeaning-othelloradius-8000.codio.io',
+  'discostone-citrushoney-8000.codio.io',
 ]
 
 
@@ -43,6 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "blog",
+    "crispy_forms",
+    "crispy_bootstrap5",
+
 ]
 
 MIDDLEWARE = [
@@ -131,3 +135,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
 from django.views.generic import ListView,DetailView,TemplateView, View
 from blog.models import Post,Comment,Tag
 from django.utils import timezone
+from blog.forms import CommentForm
 
 # Create your views here.
 
@@ -12,5 +13,21 @@ class indexView(View):
 
 
 def post_detail(request, slug):
-  post = get_object_or_404(Post,slug = slug)
-  return render(request,"blog/post_detail.html", {"post" : post})
+    post = get_object_or_404(Post,slug = slug)
+
+    if request.user.is_active:
+        if request.method == "POST":
+            comment_form = CommentForm(request.POST)
+
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.content_object = post
+                comment.creator = request.user
+                comment.save()
+                return redirect(request.path_info)
+        else:
+            comment_form = CommentForm()
+    else:
+        comment_form = None
+
+    return render(request,"blog/post_detail.html", {"post" : post, "comment_form" : comment_form})
